2a55846c7029c0c054a85ae057382522d5f43cab
kaboudieth <107319865+kaboudieth@users.noreply.github.com>
2024-02-06 15:26:02 +0100

    Update streamlit_app.py
    preview du fichier chargé (5 premières lignes) ou message d'erreur sans arreter complètement le programme

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,18 @@ def main() -> None:
     st.title("Interface IA")
     st.header("Uploader vos données")
     data = st.file_uploader("Uploader un dataset", type=["csv"])
+    
+    if data is not None:
+        try:
+            # Tentative de lecture du fichier CSV
+            data_preview = pd.read_csv(data)
+            st.success("Fichier CSV chargé avec succès!")
+            st.write("Aperçu des données chargées (5 premières lignes):")
+            st.dataframe(data_preview.head())
+        except Exception as e:
+            # S'il y a une erreur dans le formatage du fichier CSV, afficher une alerte
+            st.error(f"Une erreur s'est produite lors du chargement du fichier. Vérifiez le format du fichier CSV. \nDetails de l'erreur: {e}")
+    
     st.write("ou utiliser l'exemple ci-dessous")
     col1, col2 = st.columns([0.2, 0.8])
     with col1:
